ecfb093d78b9d94f60cf294ad78dcae292afd998
hongsonhvt <80675760+hongsonhvt@users.noreply.github.com>
2021-04-01 15:43:18 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,32 +1,46 @@
-<!DOCTYPE html>
+<?php
+$host = "ec2-23-22-191-232.compute-1.amazonaws.com";
+$port = "5432";
+$dbname = "d90g8rfgki6jb5";
+$user = "ofcrelyobnaqan";
+$password = "cefb489d75ca8fef81ab3054ee3525b3af3857ebdcc77080d96834153e2a6b94"; 
+$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
+$dbconn = pg_connect($connection_string);
+if (isset($_POST['submit'])) {
+    $fname = $_POST['firstname'];
+    $lname = $_POST['lastname'];
+    $query = pg_query($dbconn, "SELECT * FROM login WHERE email='{$fname}' AND pw='{$lname}'");
+    $login_check = pg_num_rows($query);
+    if($login_check > 0){ 
+        
+        echo "Login Successfully";    
+    }else{
+        
+        echo "Invalid Details";
+    }
+}
+?>
+<!DOCTYPE  html>
 <html>
-<head>
-  <title>login</title>
-</head>
-<body>
-<form action="action_page.php" method="post">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
+    <body>
+        <h2>Login Forms</h2>
 
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
-
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form>
-
-</body>
+        <form  method="POST">
+            Email:<br>
+            <input  type="text"  name="firstname">
+            <br>
+            Password:<br>
+            <input  type="text"  name="lastname">
+            <br><br>
+            <input  type="submit"  name="submit">
+        </form>
+    <h2>Thông tin tài khoản</h2>
+	<?php
+$result = pg_query($dbconn,"SELECT * FROM login");
+echo "<table>";
+while($row=pg_fetch_assoc($result)){echo "<tr>";
+echo "<td align='center' width='200'>" . $row['email'] . "</td>";
+echo "<td align='center' width='200'>" . $row['pw'] . "</td>";
+echo "</tr>";}echo "</table>";?>
+    </body>
 </html>
