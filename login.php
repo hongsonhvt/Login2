232b2dc15e97ab9f1aa6dea9f1bbcdf56f0cf13f
hongsonhvt <80675760+hongsonhvt@users.noreply.github.com>
2021-04-01 16:13:37 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
 <?php
-$host = "ec2-23-22-191-232.compute-1.amazonaws.com";
+$host = "ec2-107-22-245-82.compute-1.amazonaws.com";
 $port = "5432";
-$dbname = "d90g8rfgki6jb5";
-$user = "ofcrelyobnaqan";
-$password = "cefb489d75ca8fef81ab3054ee3525b3af3857ebdcc77080d96834153e2a6b94"; 
+$dbname = "dcpmvj6udn2uga";
+$user = "nnmnxzwmykzeyk";
+$password = "
+1c0f7244c61917b03c1faddbaf81fc309d87e00ffe53304f4065d239bee67f83"; 
 $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
 $dbconn = pg_connect($connection_string);
 if (isset($_POST['submit'])) {
@@ -34,13 +35,6 @@ if (isset($_POST['submit'])) {
             <br><br>
             <input  type="submit"  name="submit">
         </form>
-    <h2>Thông tin tài khoản</h2>
-	<?php
-$result = pg_query($dbconn,"SELECT * FROM login");
-echo "<table>";
-while($row=pg_fetch_assoc($result)){echo "<tr>";
-echo "<td align='center' width='200'>" . $row['email'] . "</td>";
-echo "<td align='center' width='200'>" . $row['pw'] . "</td>";
-echo "</tr>";}echo "</table>";?>
+    
     </body>
 </html>
